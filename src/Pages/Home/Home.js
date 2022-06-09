ccdcabe616a8d1166c5b99dcecbccd6f03133e9f
proalamin-247 <pro.alamin247@gmail.com>
2022-06-09 18:57:15 +0600

    top img added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,10 +3,12 @@ import Footer from '../Sheard/Footer/Footer';
 import ClaimArea from './ClaimArea/ClaimArea';
 import NewsLetter from './NewsLetter/NewsLetter';
 import './Home.css'
+import TopImg from './TopImg/TopImg';
 
 const Home = () => {
     return (
         <div>
+            <TopImg></TopImg>
             <ClaimArea></ClaimArea>
             <NewsLetter></NewsLetter> 
             <div class="divider dd"></div> 
---
+++ src/Pages/Home/TopImg/TopImg.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './TopImg.css';
+
+const TopImg = () => {
+    return (
+        <div className=''>
+            <div class="hero_top">
+                <div class="hero_top_img hero_top"></div>
+            </div>
+        </div>
+    );
+};
+
+export default TopImg;
