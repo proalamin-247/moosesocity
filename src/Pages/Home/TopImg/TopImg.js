c399e50b5f6cc5ecd4a3396352c7e5534472ec35
proalamin-247 <pro.alamin247@gmail.com>
2022-06-09 19:16:02 +0600

    top img resize

--- src/Pages/Home/TopImg/TopImg.js
+++ src/Pages/Home/TopImg/TopImg.js
@@ -4,8 +4,8 @@ import './TopImg.css';
 const TopImg = () => {
     return (
         <div className=''>
-            <div class="hero_top">
-                <div class="hero_top_img hero_top"></div>
+            <div class="hero_top mt-14 lg:mt-20">
+                <div class="hero_top_img pt-28  lg:pt-52 px-0 pb-0 lg:pb-8"></div>
             </div>
         </div>
     );
