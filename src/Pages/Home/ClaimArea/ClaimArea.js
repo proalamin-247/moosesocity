34321f0c82932f42b923ecb230fd1fdf95d0e695
proalamin-247 <pro.alamin247@gmail.com>
2022-06-09 16:28:26 +0600

    ract router, tailwind, daisy ui install and navbar create

---
+++ src/Pages/Home/ClaimArea/ClaimArea.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClaimArea = () => {
+    return (
+        <div>
+            <h1>this is claim area</h1>
+        </div>
+    );
+};
+
+export default ClaimArea;
